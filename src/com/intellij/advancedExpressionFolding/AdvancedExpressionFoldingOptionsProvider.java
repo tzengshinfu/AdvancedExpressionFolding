e28c3c7e436419d05a4feb1bff99f079170b10d1
tzengshinfu <tzengshinfu@gmail.com>
2025-01-23 16:57:56 +0800

    Added plugin title in Settings

--- src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingOptionsProvider.java
+++ src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingOptionsProvider.java
@@ -1,24 +1,12 @@
 package com.intellij.advancedExpressionFolding;
 
+import com.intellij.ide.plugins.PluginManagerCore;
+
 public class AdvancedExpressionFoldingOptionsProvider extends com.intellij.openapi.options.BeanConfigurable<AdvancedExpressionFoldingSettings.State> implements com.intellij.application.options.editor.CodeFoldingOptionsProvider {
     protected AdvancedExpressionFoldingOptionsProvider() {
         super(AdvancedExpressionFoldingSettings.getInstance().getState());
         AdvancedExpressionFoldingSettings settings = AdvancedExpressionFoldingSettings.getInstance();
-        checkBox("arithmeticExpressionsCollapse", "Math, BigDecimal and BigInteger expressions (deprecated)");
-        checkBox("concatenationExpressionsCollapse", "StringBuilder.append and Collection.add/remove expressions, interpolated Strings and Stream expressions");
-        checkBox("slicingExpressionsCollapse", "List.subList and String.substring expressions");
-        checkBox("comparingExpressionsCollapse", "Object.equals and Comparable.compareTo expressions");
-        checkBox("getExpressionsCollapse", "List.get, List.set, Map.get and Map.put expressions, array and list literals");
-        checkBox("rangeExpressionsCollapse", "For loops, range expressions");
-        checkBox("checkExpressionsCollapse", "Null safe calls");
-        checkBox("castExpressionsCollapse", "Type cast expressions");
-        checkBox("varExpressionsCollapse", "Variable declarations");
-        checkBox("getSetExpressionsCollapse", "Getters and setters");
-        checkBox("controlFlowSingleStatementCodeBlockCollapse", "Control flow single-statement code block braces (read-only files)");
-        checkBox("controlFlowMultiStatementCodeBlockCollapse", "Control flow multi-statement code block braces (read-only files, deprecated)");
-        checkBox("compactControlFlowSyntaxCollapse", "Compact control flow condition syntax");
-        checkBox("semicolonsCollapse", "Semicolons (read-only files)");
-        checkBox("assertsCollapse", "Asserts");
+        setTitle(PluginManagerCore.getPluginDescriptorOrPlatformByClassName(getClass().getName()).getName());
         checkBox("Math, BigDecimal and BigInteger expressions (deprecated)", () -> settings.getState().isArithmeticExpressionsCollapse(), aBoolean -> settings.getState().setArithmeticExpressionsCollapse(aBoolean));
         checkBox("StringBuilder.append and Collection.add/remove expressions, interpolated Strings and Stream expressions", () -> settings.getState().isConcatenationExpressionsCollapse(), aBoolean -> settings.getState().setConcatenationExpressionsCollapse(aBoolean));
         checkBox("List.subList and String.substring expressions", () -> settings.getState().isSlicingExpressionsCollapse(), aBoolean -> settings.getState().setSlicingExpressionsCollapse(aBoolean));
