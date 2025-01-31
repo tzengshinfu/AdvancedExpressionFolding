b0354fd0a143c70d6a3994ffacf3ba5071cb3f00
tzengshinfu <tzengshinfu@gmail.com>
2025-01-31 15:06:29 +0800

    docs: Added comments

--- src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingPlusCompletionContributor.java
+++ src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingPlusCompletionContributor.java
@@ -11,45 +11,69 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 
+/**
+ * Advanced Expression Folding Plus Completion Contributor
+ * Provides code completion suggestions for operator method invocations
+ */
 public class AdvancedExpressionFoldingPlusCompletionContributor extends CompletionContributor {
+    // Get instance of plugin settings
     private final AdvancedExpressionFoldingSettings settings = AdvancedExpressionFoldingSettings.getInstance();
+    // Get instance of operator method mapper
     private final AdvancedExpressionFoldingPlusOperatorMethodMapper mapper = AdvancedExpressionFoldingPlusOperatorMethodMapper.getInstance();
 
+    /**
+     * Fill completion suggestions
+     *
+     * @param parameters Completion parameters
+     * @param result     Completion result set
+     */
     public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
-        //region Do not handle general auto-completion actions
+        //region Do not handle general auto-completion actions triggered by dot
         if (parameters.getEditor().getDocument().getText(TextRange.create(parameters.getOffset() - 1, parameters.getOffset())).equals(".")) {
             return;
         }
         //endregion
 
+        // Return if operator method invoke feature is disabled
         if (!settings.getState().isOperatorMethodInvoke()) {
             return;
         }
 
+        // Return if no method is mapped
         if (mapper.getMethod() == null) {
             return;
         }
 
+        // Stop processing other completion contributors
         result.stopHere();
 
+        // Generate method parameter list string
         String arguments = String.join(", ", Arrays.stream(mapper.getMethod().getParameters()).map(p -> {
             return ((PsiType) p.getType()).getPresentableText() + " " + p.getName();
         }).toList());
 
+        // Create completion lookup element
         LookupElement item = LookupElementBuilder.create("").withBoldness(true)
-                .withIcon(Icons.METHOD_ICON)
-                .withPresentableText(mapper.getMethod().getName())
-                .withCaseSensitivity(true)
-                .withTypeText(mapper.getMethod().getReturnType().getPresentableText())
-                .withTailText("(" + arguments + ")", true)
-                .withPsiElement(mapper.getMethod())
-                .withAutoCompletionPolicy(AutoCompletionPolicy.GIVE_CHANCE_TO_OVERWRITE);
+                .withIcon(Icons.METHOD_ICON)                        // Set method icon
+                .withPresentableText(mapper.getMethod().getName())  // Set display text
+                .withCaseSensitivity(true)                         // Enable case sensitivity
+                .withTypeText(mapper.getMethod().getReturnType().getPresentableText())  // Set return type text
+                .withTailText("(" + arguments + ")", true)         // Set tail text (parameter list)
+                .withPsiElement(mapper.getMethod())                // Associate PSI element
+                .withAutoCompletionPolicy(AutoCompletionPolicy.GIVE_CHANCE_TO_OVERWRITE);  // Set auto-completion policy
         result.addElement(item);
 
+        // Get active lookup window
         Lookup lookup = LookupManager.getActiveLookup(parameters.getEditor());
 
+        // Add custom listener if lookup exists
         if (lookup != null) {
-            lookup.addLookupListener(new AdvancedExpressionFoldingPlusMultipleListener(parameters.getEditor().getProject(), parameters.getEditor(), parameters.getEditor().getDocument(), parameters.getOffset()));
+            lookup.addLookupListener(new AdvancedExpressionFoldingPlusMultipleListener(
+                    parameters.getEditor().getProject(),
+                    parameters.getEditor(),
+                    parameters.getEditor().getDocument(),
+                    parameters.getOffset()
+            ));
         }
     }
 }
